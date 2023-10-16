09b90442a4bd0ee283d2586f6d88fe06e77816db
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-16 16:55:21 +0530

    index.js changes

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 
 // to get everything which is mentioned in the app folder.
 const app = require('./app');
+
+//create a port for the server
+const port = 3000;
+
+// get request for route
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+});
+
+app.listen(port, ()=>{
+    console.log('Server Listening on port http://localhost:' + port);
+
+});
