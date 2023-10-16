216e40707a0f5ccdf6dd64b5bad94089a8a10381
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-16 22:49:56 +0530

    minor changes

--- model/user.model.js
+++ model/user.model.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const bcrypt = require('bcrypt');
-=======
->>>>>>> f6ea32e120fddb380ca1c9a5a846060379773f15
 const db = require('../config/database');
 
 // create user schema
@@ -21,19 +18,16 @@ const userSchema = new Schema({
     }
 });
 
-<<<<<<< HEAD
 userSchema.pre('save', async function(){
     try {
         var user = this;
         const salt = await (bcrypt.genSalt(10));
         const hashpass = await bcrypt.hash(user.password, salt);
-        user.password = hashpass; 
+        user.password = hashpass;
     } catch (error) {
         throw error;
     }
 })
-=======
->>>>>>> f6ea32e120fddb380ca1c9a5a846060379773f15
 const userModal = db.model('user', userSchema);
 
 module.exports = userModal;
