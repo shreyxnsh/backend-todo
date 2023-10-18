bbed2f752dbd804bb523a11a7d865b8363ef4591
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-18 20:58:12 +0530

    checkUser function

--- services/user.services.js
+++ services/user.services.js
@@ -1,6 +1,7 @@
 const userModal = require('../model/user.model');
 
 class UserService{
+    // register a new user
     static async registerUser(email,password){
         try {
             const creatUser = new userModal({email,password});
@@ -9,6 +10,16 @@ class UserService{
             throw error;
         }
     };
+
+    // function to check if user is registered in the db
+    static async checkUser(email){
+        try {
+            return await userModal({email});
+        } catch (error) {
+            throw error;
+        }
+    };
+
 }
 
 module.exports = UserService;
