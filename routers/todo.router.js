141028bba56da8714f7f598d64abaf83be584dbd
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-22 11:40:25 +0530

    fixed route

--- routers/todo.router.js
+++ routers/todo.router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const todoController = require('../controller/todo.controller');
+const todoModel = require('../model/todo.model');
 
 //create todo task
 router.post('/createtodo', todoController.createTodo );
 
-//get all the todo tasks of the user
-router.get('/getUserTodo', todoController.getUserTodo );
+// get all the todo tasks of the user with userId as query parameter
+router.get('/getUserTodo', todoController.getUserTodo);
+
 module.exports = router;
